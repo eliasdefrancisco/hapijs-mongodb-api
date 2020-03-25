61fc2f54750b473f82b5605c07ca5a32dce9f4bc
Elias de Francisco <elias@iMac-de-Elias.local>
2020-03-25 18:53:59 +0100

    minor fix

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/testdb', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useFindAndModify: true
+    useFindAndModify: false
 })
 .then(() => console.log('Database connected'))
 .catch(err => console.log(ErrorEvent))
